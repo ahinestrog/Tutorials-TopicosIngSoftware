7ac2678a2498a4ca15b4812cdf2a2c2f460be7c5
Alejandro Hinestroza <ahinestrog@eafit.edu.co>
2025-02-09 13:39:30 -0500

    Tutorial2

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponseRedirect 
-from django.views.generic import TemplateView 
+from django.views.generic import TemplateView, ListView
 from django.views import View
 from django import forms
+from .models import Product
 
 # Create your views here.
 #def homePageView(request):
@@ -21,7 +22,7 @@ class AboutPageView(TemplateView):
             "title": "About Us - Online Store",
             "subtitle": "About Us",
             "description": "This is an about page...",
-            "author": "Develop by: Alejandro"
+            "author": "Develop by: Alejandro Hinestroza"
         })
 
         return context
@@ -43,13 +44,13 @@ class ContactPageView(TemplateView):
 
         return context
 
-class Product:
+"""class Product:
     products = [
         {"id": "1", "name": "TV", "description": "Best TV", "price": 1000},
         {"id": "2", "name": "iPhone", "description": "Best iPhone", "price": 1500},
         {"id": "3", "name": "Chromecast", "description": "Best Chromecast", "price": 2000},
         {"id": "4", "name": "Glasses", "description": "Best Glasses", "price": 500},
-    ]
+    ]"""
 
 class ProductIndexView(View):
     template_name = 'products/index.html'
@@ -58,27 +59,36 @@ class ProductIndexView(View):
         viewData = {}
         viewData["title"] = "Products - Online Store"
         viewData["subtitle"] = "List of products"
-        viewData["products"] = Product.products
+        viewData["products"] = Product.objects.all()
         return render(request, self.template_name, viewData)
 
 class ProductShowView(View):
     template_name = 'products/show.html'
 
     def get(self, request, id):
-        viewData = {}
+        
         try:
-            product = Product.products[int(id) - 1]
+            product_id = int(id)
+            if product_id < 1:
+                raise ValueError("Product id must be 1 or greater")
+            product = get_object_or_404(Product, pk=product_id)
         except (IndexError, ValueError):  # Para cuando el indice no es valido
             return HttpResponseRedirect('/')  # Lo redirige al home
-
-        viewData["title"] = product["name"] + " - Online Store"
-        viewData["subtitle"] = product["name"] + " - Product information"
+        
+        viewData = {}
+        product = get_object_or_404(Product, pk=product_id)
+        viewData["title"] = product.name + " - Online Store"
+        viewData["subtitle"] = product.name + " - Product information"
         viewData["product"] = product
         return render(request, self.template_name, viewData)
 
-class ProductForm(forms.Form):
-    name = forms.CharField(required=True)
-    price = forms.FloatField(required=True)
+class ProductForm(forms.ModelForm):
+    #name = forms.CharField(required=True)
+    #price = forms.FloatField(required=True)
+
+    class Meta:
+        model = Product
+        fields = ['name', 'price']
 
     def clean_price(self):
         price = self.cleaned_data.get('price')
@@ -99,6 +109,7 @@ class ProductCreateView(View):
     def post(self, request):
         form = ProductForm(request.POST)
         if form.is_valid():
+            form.save()
             return redirect('product_created')
         else:
             viewData = {}
@@ -107,3 +118,14 @@ class ProductCreateView(View):
 
 class ProductCreatedView(TemplateView):
         template_name = 'products/product_created.html'
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'product_list.html'
+    context_object_name = 'products' # This will allow you to loop through "products" in the template
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Products - Online Store'
+        context['subtitle'] = 'List of products'
+        return context
